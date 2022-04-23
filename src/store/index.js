caed2cc038f4c8ee8e9b23fff9748ea76c13d7ee
Mitko Georgiev <omnichronous@gmail.com>
2022-04-23 13:08:48 +0200

    table style, layout and user input

--- src/store/index.js
+++ src/store/index.js
@@ -1,14 +1,19 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import { merge } from 'lodash'
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    quantities: {},
   },
   getters: {
   },
   mutations: {
+    SET_QUANTITIES (state, quantities) {
+      Vue.set(state, 'quantities', merge({}, state.quantities, quantities))
+    },
   },
   actions: {
   },
